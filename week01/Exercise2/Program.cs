e968c17a265fb9285bfba48954d306718c252aa8
0rregol <83845391+0rregol@users.noreply.github.com>
2025-01-17 17:12:30 -0300

    Update Program.cs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?.");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+        
+        string letter = "";
+        
+        if (grade>=90)
+        {
+            letter="A";
+        }
+        if((grade>=80)&&(grade<90))
+        {
+            letter="B";
+        }
+         if((grade>=70)&&(grade<80))
+        {
+            letter="C";
+        }
+         if((grade>=60)&&(grade<70))
+        {
+            letter="D";
+        }
+         if(grade<60)
+        {
+            letter="F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+         if(grade>=70)
+        {
+            Console.WriteLine("Congratulations on passing the course!.");
+        }
+        else
+        {
+            Console.WriteLine("Don't be discouraged, just try harder next time!.");
+        }
     }
 }
