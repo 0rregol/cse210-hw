9b97714186e1da2b13393935a5452623ca330fc0
0rregol <83845391+0rregol@users.noreply.github.com>
2025-01-17 17:41:16 -0300

    Update Program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random numberGenerator = new Random();
+        int magicNumber = numberGenerator.Next(1,101);
+        int number = -1;
+
+        while(number!=magicNumber)
+        {
+            Console.Write("What is your guess?.");
+            number = int.Parse(Console.ReadLine());
+            if(magicNumber>number)
+            {
+                Console.WriteLine("Higher");
+            }
+            if(magicNumber<number)
+            {
+                Console.WriteLine("Lower");
+            }
+            if(magicNumber==number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
+       
     }
 }
