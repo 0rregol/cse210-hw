5b7443ac3495b11d2ca299b6a5bbb84e1ea23c44
0rregol <83845391+0rregol@users.noreply.github.com>
2025-01-27 16:41:19 -0300

    Create Journal.cs

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+
+public class Journal
+{
+    
+    public List<Entry> Entries { get; set; }
+
+   
+    public Journal()
+    {
+        Entries = new List<Entry>();
+    }
+
+   
+    public void AddEntry(Entry entry)
+    {
+        Entries.Add(entry); 
+    }
+
+    
+    public void DisplayAll()
+    {
+        foreach (var entry in Entries) 
+        {
+            entry.Display(); 
+        }
+    }
+
+    
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter outputFile = new StreamWriter(filename)) 
+        {
+            foreach (var entry in Entries) 
+            {
+                
+                outputFile.WriteLine($"{entry.Date}|{entry.Prompt}|{entry.Response}");
+            }
+        }
+    }
+
+   
+    public void LoadFromFile(string filename)
+    {
+        Entries.Clear(); 
+        string[] lines = File.ReadAllLines(filename); 
+
+        foreach (string line in lines) 
+        {
+            string[] parts = line.Split('|'); 
+            Entry entry = new Entry(parts[1], parts[2], parts[0]);
+            Entries.Add(entry); 
+        }
+    }
+}
