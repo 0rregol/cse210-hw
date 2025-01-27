4c75d14fe7dc79c4cc87c15c8f20d4fb0c1d0c95
0rregol <83845391+0rregol@users.noreply.github.com>
2025-01-27 17:13:56 -0300

    Update Entry.cs

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -3,7 +3,8 @@ public class Entry
    
     public string Prompt { get; set; }  
     public string Response { get; set; } 
-    public string Date { get; set; }     
+    public string Date { get; set; }    
+     public string Mood { get; set; } 
 
    
     public Entry(string prompt, string response, string date)
