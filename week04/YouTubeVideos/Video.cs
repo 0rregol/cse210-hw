92a160d42be078b507f534a1986a1d1a8051c847
0rregol <83845391+0rregol@users.noreply.github.com>
2025-02-04 23:42:31 -0300

    Create Video.cs

---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,39 @@
+public class Video
+{
+    public string Title { get; set; }  
+    public string Author { get; set; }
+    public int Length { get; set; }   
+    private List<Comment> Comments { get; set; } 
+
+    public Video(string title, string author, int length)
+    {
+        Title = title;
+        Author = author;
+        Length = length;
+        Comments = new List<Comment>();
+    }
+
+    public void AddComment(Comment comment)
+    {
+        Comments.Add(comment);
+    }
+
+    public int GetNumberOfComments()
+    {
+        return Comments.Count;
+    }
+
+    public void DisplayVideoDetails()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Length: {Length} seconds");
+        Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
+        Console.WriteLine("Comments:");
+        foreach (var comment in Comments)
+        {
+            Console.WriteLine($"- {comment.CommenterName}: {comment.CommentText}");
+        }
+        Console.WriteLine();
+    }
+}
