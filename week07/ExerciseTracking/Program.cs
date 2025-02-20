c7ec16e2840aa164edf903227347ad0d267c2071
0rregol <83845391+0rregol@users.noreply.github.com>
2025-02-20 10:16:03 -0300

    Add files via upload

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Activity running = new Running(new DateTime(2022, 11, 3), 30, 3.0);
+        Activity cycling = new Cycling(new DateTime(2024, 11, 26), 30, 15.0);
+        Activity swimming = new Swimming(new DateTime(2025, 02, 20), 30, 20);
+
+        List<Activity> activities = new List<Activity> { running, cycling, swimming };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
